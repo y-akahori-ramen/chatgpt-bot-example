18afce2dcce614fd0feeaabfd08d0eb3950227d3
y-akahori-ramen <y.akahori.ramen@gmail.com>
2023-03-27 12:26:01 +0900

    Add config file. Add error handling.

--- dist/index.js
+++ dist/index.js
@@ -6,14 +6,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const bolt_1 = require("@slack/bolt");
 const openai_1 = require("openai");
+const fs_1 = __importDefault(require("fs"));
 dotenv_1.default.config();
 const openai = new openai_1.OpenAIApi(new openai_1.Configuration({ apiKey: process.env['OPENAI_API_KEY'] }));
+const config = JSON.parse(fs_1.default.readFileSync('./config.json', 'utf8'));
+config.model ??= 'gpt-3.5-turbo';
+config.max_tokens ??= 2048;
+config.system_message ??= 'You are a helpful assistant.';
+config.port ??= 3000;
 const app = new bolt_1.App({
     token: process.env['SLACK_BOT_USER_OAUTH_TOKEN'],
     signingSecret: process.env['SLACK_SIGNING_SECRET'],
     socketMode: true,
     appToken: process.env['SLACK_APP_TOKEN'],
-    port: 3000
+    port: config.port
 });
 app.event('app_mention', async ({ say, event, client }) => {
     const ts = event.thread_ts ?? event.event_ts;
@@ -26,13 +32,24 @@ app.event('app_mention', async ({ say, event, client }) => {
             const role = message.bot_id === undefined ? openai_1.ChatCompletionRequestMessageRoleEnum.User : openai_1.ChatCompletionRequestMessageRoleEnum.Assistant;
             return { role: role, content: message.text ?? '' };
         });
+        messages.unshift({ role: openai_1.ChatCompletionRequestMessageRoleEnum.System, content: config.system_message });
         const request = {
-            model: 'gpt-3.5-turbo',
+            model: config.model,
             messages: messages,
-            max_tokens: 2048
+            max_tokens: config.max_tokens
         };
-        const res = await openai.createChatCompletion(request);
-        await say({ text: res.data.choices[0].message?.content, thread_ts: ts });
+        try {
+            const res = await openai.createChatCompletion(request);
+            await say({ text: res.data.choices[0].message?.content, thread_ts: ts });
+        }
+        catch (e) {
+            if (e instanceof Error) {
+                await say({ text: `Error: ${e.message}`, thread_ts: ts });
+            }
+            else {
+                await say({ text: 'Unknown Error.', thread_ts: ts });
+            }
+        }
     }
 });
 (async () => {
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
 import dotenv from 'dotenv'
 import { App } from '@slack/bolt'
 import { Configuration, OpenAIApi, CreateChatCompletionRequest, ChatCompletionRequestMessage, ChatCompletionRequestMessageRoleEnum } from 'openai';
+import fs from 'fs';
 
 dotenv.config()
 
 const openai = new OpenAIApi(new Configuration({ apiKey: process.env['OPENAI_API_KEY'] }));
+
+type Config = {
+  model: string,
+  max_tokens: number,
+  system_message: string,
+  port: number
+}
+const config: Config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+config.model ??= 'gpt-3.5-turbo';
+config.max_tokens ??= 2048;
+config.system_message ??= 'You are a helpful assistant.';
+config.port ??= 3000;
 
 const app = new App({
   token: process.env['SLACK_BOT_USER_OAUTH_TOKEN'],
   signingSecret: process.env['SLACK_SIGNING_SECRET'],
   socketMode: true,
   appToken: process.env['SLACK_APP_TOKEN'],
-  port: 3000
+  port: config.port
 });
 
 app.event('app_mention', async ({ say, event, client }) => {
@@ -25,15 +38,24 @@ app.event('app_mention', async ({ say, event, client }) => {
       const role = message.bot_id === undefined ? ChatCompletionRequestMessageRoleEnum.User : ChatCompletionRequestMessageRoleEnum.Assistant
       return { role: role, content: message.text ?? '' }
     });
+    messages.unshift({ role: ChatCompletionRequestMessageRoleEnum.System, content: config.system_message });
 
     const request: CreateChatCompletionRequest = {
-      model: 'gpt-3.5-turbo',
+      model: config.model,
       messages: messages,
-      max_tokens: 2048
-    }
+      max_tokens: config.max_tokens
+    };
 
-    const res = await openai.createChatCompletion(request);
-    await say({ text: res.data.choices[0].message?.content, thread_ts: ts });
+    try {
+      const res = await openai.createChatCompletion(request);
+      await say({ text: res.data.choices[0].message?.content, thread_ts: ts });
+    } catch (e) {
+      if (e instanceof Error) {
+        await say({ text: `Error: ${e.message}`, thread_ts: ts });
+      } else {
+        await say({ text: 'Unknown Error.', thread_ts: ts });
+      }
+    }
   }
 });
 
